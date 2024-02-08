db2a08c9d4156fc4412c0ef8b7287a31216e32c7
nicocasaisd <nicolas.casais.dassie@gmail.com>
2024-02-08 15:46:37 -0300

    add 72

--- 4.Nivel4/72_api_scraping.py
+++ 4.Nivel4/72_api_scraping.py
@@ -1,13 +1,35 @@
 import requests
+import pandas as pd
 
 headers = {
     "Referer": "https://www.udemy.com/courses/search/?p=2&q=python&src=ukw",
     "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
 }
 
-url_api = 'https://www.udemy.com/api-2.0/search-courses/?src=ukw&q=python&skip_price=true'
+cursos_totales = []
 
-response = requests.get(url_api, headers=headers)
+for i in range(1,2):
+    print(f"***********Iteracion {i}")
+    url_api = 'https://www.udemy.com/api-2.0/search-courses/?src=ukw&q=python&skip_price=true&p=' + str(i)
 
-data = response.json()
+    response = requests.get(url_api, headers=headers)
 
+    data = response.json()
+
+    cursos = data['courses']
+
+    for curso in cursos:
+        cursos_totales.append(
+            {
+                "titulo":curso['title'],
+                "num_reviews":curso['num_reviews'],
+                "rating":curso['rating']
+            }
+        )
+
+
+df = pd.DataFrame(cursos_totales)
+
+print(df)
+
+df.to_csv('cursos_udemy.csv')
